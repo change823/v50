cd32fd64294882ad602ac65b2f2e24241e75da21
yida <2413633716@qq.com>
2026-02-02 23:47:52 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -30,6 +30,7 @@ export default defineConfig({
         ]
       },
       workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/.*\.supabase\.co\/.*/i,
